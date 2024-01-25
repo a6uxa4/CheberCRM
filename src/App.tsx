45c9fb078d24947c23162b1ce2d9dbe6c2b44893
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-01-25 16:28:54 +0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
-  console.log(import.meta.env.VITE_SERVER_URL);
-  return <>HELLO</>;
+  console.log(import.meta.env.VITE_SERVER_URL)
+  return <>HELLO THIS IS PROJECT FOR ADMIN CHEBER</>
 }
 
-export default App;
+export default App
