a0bfdbc54a0fc7ba6fa027e0f8829b06571a72d0
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-01-26 13:01:45 +0600

    fix

--- src/routes/adminRoutes/index.tsx
+++ src/routes/adminRoutes/index.tsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from '../../layout'
+
+import { CalendarPage } from '../../pages/admin/Calendar'
+import { UsersPage } from '../../pages/admin/Users'
+import { SettingsPage } from '../../pages/super-admin/Settings'
+
 export const AdminRoutes = () => {
-  return <div>AdminRoutes</div>
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' element={<CalendarPage />} />
+        <Route path='/users' element={<UsersPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  )
 }
--- src/utils/constants/general.tsx
+++ src/utils/constants/general.tsx
@@ -3,6 +3,8 @@ import { HiBuildingOffice2 } from 'react-icons/hi2'
 import { IoMdSettings, IoMdInformationCircle } from 'react-icons/io'
 import { MdAnnouncement } from 'react-icons/md'
 import { BiSupport } from 'react-icons/bi'
+import { FaCalendarDays } from 'react-icons/fa6'
+import { FaUsers } from 'react-icons/fa'
 
 export const DASHBOARD_DATA = [
   [
@@ -52,6 +54,23 @@ export const DASHBOARD_DATA = [
       icon: IoMdSettings,
       name: 'Поддержка',
       navigate: '/supports'
+    }
+  ],
+  [
+    {
+      icon: FaCalendarDays,
+      name: 'Календарь',
+      navigate: '/'
+    },
+    {
+      icon: FaUsers,
+      name: 'Клиенты',
+      navigate: '/users'
+    },
+    {
+      icon: IoMdSettings,
+      name: 'Настройки',
+      navigate: '/settings'
     }
   ]
 ]
